12c83d81e964fec8b749c6ce0a95d5fada90338e
andrewhood <ahood@go1.com>
2022-12-24 12:42:37 +1000

    build static storybook

---
+++ storybook-static/745.31153155.iframe.bundle.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkgo1d_api_doc=self.webpackChunkgo1d_api_doc||[]).push([[745],{"./node_modules/react-dom/client.js":(__unused_webpack_module,exports,__webpack_require__)=>{var m=__webpack_require__("./node_modules/react-dom/index.js");exports.createRoot=m.createRoot,exports.hydrateRoot=m.hydrateRoot}}]);
